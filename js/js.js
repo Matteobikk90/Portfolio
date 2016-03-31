a2fb9539b6cf6a55157afaff6d0d091ba200e5c2
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-03-31 16:02:23 +0200

    homePageDone with jQuery

--- js/js.js
+++ js/js.js
@@ -1,1 +1,15 @@
 // alert('hello');
+// $(document).ready(function() {
+
+//   $('.contact').mouseover(function(){
+//     $(this).stop().effect('shake', {distance:3}, 200);
+// });
+
+// });
+$ (document).ready(function() {
+
+$(".contact, .about-me, .projects").mouseover(function () {
+      $(this).effect("shake");
+}); 
+
+});
